cb47573cce96a1ed378d1c17ecd6d43b9bb452e4
efrenjose <efrenrodriguez@live.com>
2018-10-14 16:53:00 -0500

    enable cors on express app

--- keystone/routes/index.js
+++ keystone/routes/index.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone');
 var importRoutes = keystone.importer(__dirname);
+var cors = require('cors');
 
 var routes = {
     views: importRoutes('./views'),
@@ -7,6 +8,14 @@ var routes = {
 }
 
 exports = module.exports = function(app) {
+    app.use(cors());
+    // app.use(function(req, res, next) {
+    //     res.header("Access-Control-Allow-Origin", "*");
+    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    //     next();
+    //   });
+
     app.get('/', routes.views.index);
     app.get('/add-event', routes.views.addEvent);
     app.post('/add-event', routes.api.event.post)
